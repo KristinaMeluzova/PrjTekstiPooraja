7cb327faaf70b3fec81f27913439ce1e60eab352
krmelu <krmelu@ICT-50124K>
2024-02-07 14:05:31 +0200

    CTekstiPooraja ja ITeisendused on tehtud

--- PrjTekstiPooraja/CTekstiPooraja.vb
+++ PrjTekstiPooraja/CTekstiPooraja.vb
@@ -1,9 +1,14 @@
 Public Class CTekstiPooraja
     Implements ITeisendused
+
+    'Klassi CTekstiPooraja atribuudid (muutujad)
     Private intAlgusSymbol As Integer
     Private intLoppSymbol As Integer
     Private strPooratavTekst As String
 
+    'Liidese omaduste (atribuutide) seadistimamine
+
+    'Atribuut intAlgus seadistamine
     Private Property intAlgus As Integer Implements ITeisendused.intAlgus
         Get
             Return intAlgusSymbol 'atribuudi küsimusel tagasta intAlgusSymbol
@@ -13,6 +18,7 @@ Public Class CTekstiPooraja
         End Set
     End Property
 
+    'Atribuut intLopp seadistamine
     Private Property intLopp As Integer Implements ITeisendused.intLopp
         Get
             Return intLoppSymbol 'atribuudi küsimusel tagasta intLoppSymbol
@@ -22,6 +28,7 @@ Public Class CTekstiPooraja
         End Set
     End Property
 
+    'Atribuut strTekst seadistamine
     Private Property strTekst As String Implements ITeisendused.strTekst
         Get
             Return strPooratavTekst 'atribuudi küsimusel tagasta strPooratavTekst
@@ -31,11 +38,18 @@ Public Class CTekstiPooraja
         End Set
     End Property
 
+    'Meetod, mis pöörab tagurpidi
+    'sisendparamteetrina strSisendTekst antud avaldise
+    'ja salvestab väärtuse atribuuti strPooratavTekst
+
     Private Sub teisendaTekst(ByRef strSisendTekst As String) _
         Implements ITeisendused.teisendaTekst
         strPooratavTekst = StrReverse(strSisendTekst)
     End Sub
 
+    'Meetod, mis võtab atribuunist
+    'strSisendTekst väärtuse
+    'ja pöörab selle stringi tagurpidi
     Private Function pooraTekst() As String _
         Implements ITeisendused.pooraTekst
 
