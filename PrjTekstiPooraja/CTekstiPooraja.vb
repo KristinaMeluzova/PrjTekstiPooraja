ec117ef5d2ad02d72e5934148a1c69140925f950
krmelu <krmelu@ICT-50124K>
2024-02-07 13:50:39 +0200

    ITteisendusd, CTekstiPooraja peeagu tehtud

--- PrjTekstiPooraja/CTekstiPooraja.vb
+++ PrjTekstiPooraja/CTekstiPooraja.vb
@@ -1,3 +1,44 @@
 Public Class CTekstiPooraja
+    Implements ITeisendused
+    Private intAlgusSymbol As Integer
+    Private intLoppSymbol As Integer
+    Private strPooratavTekst As String
 
+    Private Property intAlgus As Integer Implements ITeisendused.intAlgus
+        Get
+            Return intAlgusSymbol 'atribuudi küsimusel tagasta intAlgusSymbol
+        End Get
+        Set(value As Integer)
+            intAlgusSymbol = value 'atribuudi väärtuste seadistamine
+        End Set
+    End Property
+
+    Private Property intLopp As Integer Implements ITeisendused.intLopp
+        Get
+            Return intLoppSymbol 'atribuudi küsimusel tagasta intLoppSymbol
+        End Get
+        Set(value As Integer)
+            intLoppSymbol = value 'atribuudi väärtuste seadistamine
+        End Set
+    End Property
+
+    Private Property strTekst As String Implements ITeisendused.strTekst
+        Get
+            Return strPooratavTekst 'atribuudi küsimusel tagasta strPooratavTekst
+        End Get
+        Set(value As String)
+            strPooratavTekst = value 'atribuudi väärtuste seadistamine
+        End Set
+    End Property
+
+    Private Sub teisendaTekst(ByRef strSisendTekst As String) _
+        Implements ITeisendused.teisendaTekst
+        strPooratavTekst = StrReverse(strSisendTekst)
+    End Sub
+
+    Private Function pooraTekst() As String _
+        Implements ITeisendused.pooraTekst
+
+        Return StrReverse(strPooratavTekst)
+    End Function
 End Class
---
+++ PrjTekstiPooraja/ITeisendused.vb
@@ -0,0 +1,8 @@
+﻿Public Interface ITeisendused
+    Property intAlgus As Integer    'Teksti esimese sümboli ASCII koodi jaoks
+    Property intLopp As Integer     'Teksti esimese sümboli ASCII koodi jaoks
+    Property strTekst As String     'kasutaja sisestatud teksti jaoks
+
+    Function pooraTekst() As String       'liidese meetodid koodi
+    Sub teisendaTekst(ByRef strSisendTekst As String)   'ei oma
+End Interface
