95ae0893e0f8c3be21fd1d84cad206c477fa2d65
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-11 17:09:37 +0200

    on lisatud klass CAlgoritmilinePooraja

--- PrjTekstiPooraja/CTekstiPooraja.vb
+++ PrjTekstiPooraja/CTekstiPooraja.vb
@@ -56,3 +56,42 @@ Public Class CTekstiPooraja
         Return StrReverse(strPooratavTekst)
     End Function
 End Class
+
+Public Class CAlgoritmilinePooraja
+    Implements ITeisendused
+
+    Private Property intAlgus As Integer Implements ITeisendused.intAlgus
+        Get
+
+        End Get
+        Set(value As Integer)
+
+        End Set
+    End Property
+
+    Private Property intLopp As Integer Implements ITeisendused.intLopp
+        Get
+
+        End Get
+        Set(value As Integer)
+
+        End Set
+    End Property
+
+    Private Property strTekst As String Implements ITeisendused.strTekst
+        Get
+
+        End Get
+        Set(value As String)
+
+        End Set
+    End Property
+
+    Private Sub teisendaTekst(ByRef strSisendTekst As String) Implements ITeisendused.teisendaTekst
+
+    End Sub
+
+    Private Function pooraTekst() As String Implements ITeisendused.pooraTekst
+
+    End Function
+End Class
